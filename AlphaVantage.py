fa5e550fdf83474e899f485fa5a13d2020e3a514
Peter Juett <pjuett@yahoo.com>
2018-04-03 20:40:14 +0800

    added header

--- AlphaVantage.py
+++ AlphaVantage.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+#
+# Module for receiving stock and currency prices from Alphavantage
+#
+# by Peter Juett
+# References: https://www.alphavantage.co/
+#
+# Copyright 2018
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import urllib2, urllib, json
 import time
 import datetime
