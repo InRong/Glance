37b96292ad137abbd261f0bb83042aa78d8e845b
Peter Juett <pjuett@yahoo.com>
2018-04-05 22:15:33 +0800

    enhanced events so they do not need to be continuous

--- Event.py
+++ Event.py
@@ -108,52 +108,50 @@ class EventList(object):
 	        while (x < NUMBER_OF_EVENTS):
                 	event_time = db.get_value("event" + str(x) + "time")
 
-	                if len(event_time)<2:
-        	                break
+	                if len(event_time)>2:
 
-	                event = db.get_value("event" + str(x) + "days").lower()
-	                event_description = db.get_value("event" + str(x) + "description")
+		                event = db.get_value("event" + str(x) + "days").lower()
+		                event_description = db.get_value("event" + str(x) + "description")
 
-        	        if len(event)<2:
-                	        break
+	        	        if len(event)>2:
 
-	                event_hours = int(event_time[:2]) #hours
-        	        event_mins =  int(event_time[-2:]) #mins
+			                event_hours = int(event_time[:2]) #hours
+        			        event_mins =  int(event_time[-2:]) #mins
 
-                	if "mon" in event or "weekday" in event or "everyday" in event:
-                        	week_mins = (0 * 24 * 60) + (event_hours*60) + event_mins
-	                        my_event  = Event(x,event_hours,event_mins, week_mins, "Mon", event_description)
-        	                self.event_list.append(my_event)
+		                	if "mon" in event or "weekday" in event or "everyday" in event:
+                		        	week_mins = (0 * 24 * 60) + (event_hours*60) + event_mins
+			                        my_event  = Event(x,event_hours,event_mins, week_mins, "Mon", event_description)
+        			                self.event_list.append(my_event)
 
-                	if "tue" in event or "weekday" in event or "everyday" in event:
-                        	week_mins = (1 * 24 * 60) + (event_hours*60) + event_mins
-	                        my_event  = Event(x,event_hours,event_mins, week_mins, "Tue", event_description)
-        	                self.event_list.append(my_event)
+		                	if "tue" in event or "weekday" in event or "everyday" in event:
+                		        	week_mins = (1 * 24 * 60) + (event_hours*60) + event_mins
+	                        		my_event  = Event(x,event_hours,event_mins, week_mins, "Tue", event_description)
+		        	                self.event_list.append(my_event)
 
-                	if "wed" in event or "weekday" in event or "everyday" in event:
-                        	week_mins = (2 * 24 * 60) + (event_hours*60) + event_mins
-	                        my_event  = Event(x,event_hours,event_mins, week_mins, "Wed", event_description)
-        	                self.event_list.append(my_event)
+                			if "wed" in event or "weekday" in event or "everyday" in event:
+		                        	week_mins = (2 * 24 * 60) + (event_hours*60) + event_mins
+	        		                my_event  = Event(x,event_hours,event_mins, week_mins, "Wed", event_description)
+        	                		self.event_list.append(my_event)
 
-                	if "thu" in event or "weekday" in event or "everyday" in event:
-                        	week_mins = (3 * 24 * 60) + (event_hours*60) + event_mins
-	                        my_event  = Event(x,event_hours,event_mins, week_mins, "Thu", event_description)
-        	                self.event_list.append(my_event)
+		                	if "thu" in event or "weekday" in event or "everyday" in event:
+                		        	week_mins = (3 * 24 * 60) + (event_hours*60) + event_mins
+	                        		my_event  = Event(x,event_hours,event_mins, week_mins, "Thu", event_description)
+		        	                self.event_list.append(my_event)
 
-                	if "fri" in event or "weekday" in event or "everyday" in event:
-                        	week_mins = (4 * 24 * 60) + (event_hours*60) + event_mins
-	                        my_event  = Event(x,event_hours,event_mins, week_mins, "Fri", event_description)
-        	                self.event_list.append(my_event)
+		                	if "fri" in event or "weekday" in event or "everyday" in event:
+                		        	week_mins = (4 * 24 * 60) + (event_hours*60) + event_mins
+	                        		my_event  = Event(x,event_hours,event_mins, week_mins, "Fri", event_description)
+		        	                self.event_list.append(my_event)
 
-        	        if "sat" in event or "weekend" in event or "everyday" in event:
-                        	week_mins = (5 * 24 * 60) + (event_hours*60) + event_mins
-	                        my_event  = Event(x,event_hours,event_mins, week_mins, "Sat", event_description)
-        	                self.event_list.append(my_event)
+		        	        if "sat" in event or "weekend" in event or "everyday" in event:
+                		        	week_mins = (5 * 24 * 60) + (event_hours*60) + event_mins
+			                        my_event  = Event(x,event_hours,event_mins, week_mins, "Sat", event_description)
+		        	                self.event_list.append(my_event)
 
-        	        if "sun" in event or "weekend" in event or "everyday" in event:
-                        	week_mins = (6 * 24 * 60) + (event_hours*60) + event_mins
-	                        my_event  = Event(x,event_hours,event_mins, week_mins, "Sun", event_description)
-        	                self.event_list.append(my_event)
+		        	        if "sun" in event or "weekend" in event or "everyday" in event:
+                		        	week_mins = (6 * 24 * 60) + (event_hours*60) + event_mins
+			                        my_event  = Event(x,event_hours,event_mins, week_mins, "Sun", event_description)
+		        	                self.event_list.append(my_event)
 
 	                x = x + 1
 
@@ -177,6 +175,9 @@ if __name__ == "__main__":
 	db.load_settings()
         event_list = EventList()
         next_event = event_list.get_next_event(db)
-        print "Next Event: " + next_event.day + " " + next_event.get_time(db.get_value("timestyle")=="24") + " " + next_event.description
+	if next_event == None:
+		print ("No events")
+	else:
+	        print "Next Event: " + next_event.day + " " + next_event.get_time(db.get_value("timestyle")=="24") + " " + next_event.description
 
 
